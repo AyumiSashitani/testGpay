71051fe492f3bfbb37445b1ef206267c13374ba0
sasitani <sasitani_611@yahoo.co.jp>
2021-01-21 23:57:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/testgpay/MainActivity.kt
@@ -0,0 +1,218 @@
+package com.example.testgpay
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testgpay.util.Json
+import com.example.testgpay.util.PaymentsUtil
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.wallet.*
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val shippingCost = (90 * 1000000).toLong()
+    private val LOAD_PAYMENT_DATA_REQUEST_CODE = 991
+
+//    private val SHIPPING_COST_CENTS: Long = 90 * PaymentsUtil.CENTS_IN_A_UNIT.longValue()
+
+    private lateinit var paymentsClient: PaymentsClient
+
+    private lateinit var garmentList: JSONArray
+    private lateinit var selectedGarment: JSONObject
+
+    private val googlePayButton: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Set up the mock information for our item in the UI.
+//        selectedGarment = fetchRandomGarment()
+//        displayGarment(selectedGarment)
+
+        paymentsClient = PaymentsUtil.createPaymentsClient(this)
+        possiblyShowGooglePayButton()
+
+        googlePayButton?.setOnClickListener { requestPayment() }
+
+//        var paymentsClient: PaymentsClient = service.createPaymentsClient(this)
+//
+//        var readyToPayRequest: IsReadyToPayRequest =
+//            IsReadyToPayRequest.fromJson(service.cardPaymentMethod().toString())
+//
+//
+//
+//        var task :Task<Boolean> = paymentsClient.isReadyToPay(readyToPayRequest)
+//        task.addOnCompleteListener(this, OnCompleteListener {
+//        })
+    }
+
+    private fun possiblyShowGooglePayButton() {
+
+        val isReadyToPayJson = PaymentsUtil.isReadyToPayRequest() ?: return
+        val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
+        val task = paymentsClient.isReadyToPay(request)
+        task.addOnCompleteListener { completedTask ->
+            try {
+                completedTask.getResult(ApiException::class.java)?.let(::setGooglePayAvailable)
+            } catch (exception: ApiException) {
+                // Process error
+                Log.w("isReadyToPay failed", exception)
+            }
+        }
+    }
+
+    private fun setGooglePayAvailable(available: Boolean) {
+        if (available) {
+            googlePayButton?.visibility = View.VISIBLE
+        } else {
+            Toast.makeText(
+                this,
+                "Unfortunately, Google Pay is not available on this device",
+                Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    private fun requestPayment() {
+
+        // Disables the button to prevent multiple clicks.
+        googlePayButton?.isClickable = false
+
+        // The price provided to the API should include taxes and shipping.
+        // This price is not displayed to the user.
+        val garmentPrice = selectedGarment.getDouble("price")
+        val priceCents = Math.round(garmentPrice * PaymentsUtil.CENTS.toLong())
+
+        val paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(priceCents)
+        if (paymentDataRequestJson == null) {
+            Log.e("RequestPayment", "Can't fetch payment data request")
+            return
+        }
+        val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
+
+        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+        // AutoResolveHelper to wait for the user interacting with it. Once completed,
+        // onActivityResult will be called with the result.
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                paymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE
+            )
+        }
+    }
+
+    /**
+     * Handle a resolved activity from the Google Pay payment sheet.
+     *
+     * @param requestCode Request code originally supplied to AutoResolveHelper in requestPayment().
+     * @param resultCode Result code returned by the Google Pay API.
+     * @param data Intent from the Google Pay API containing payment or error data.
+     * @see [Getting a result
+     * from an Activity](https://developer.android.com/training/basics/intents/result)
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            // Value passed in AutoResolveHelper
+            LOAD_PAYMENT_DATA_REQUEST_CODE -> {
+                when (resultCode) {
+                    RESULT_OK ->
+                        data?.let { intent ->
+                            PaymentData.getFromIntent(intent)?.let(::handlePaymentSuccess)
+                        }
+
+                    RESULT_CANCELED -> {
+                        // The user cancelled the payment attempt
+                    }
+
+                    AutoResolveHelper.RESULT_ERROR -> {
+                        AutoResolveHelper.getStatusFromIntent(data)?.let {
+                            handleError(it.statusCode)
+                        }
+                    }
+                }
+
+                // Re-enables the Google Pay payment button.
+                googlePayButton?.isClickable = true
+            }
+        }
+    }
+
+    /**
+     * PaymentData response object contains the payment information, as well as any additional
+     * requested information, such as billing and shipping address.
+     *
+     * @param paymentData A response object returned by Google after a payer approves payment.
+     * @see [Payment
+     * Data](https://developers.google.com/pay/api/android/reference/object.PaymentData)
+     */
+    private fun handlePaymentSuccess(paymentData: PaymentData) {
+        val paymentInformation = paymentData.toJson() ?: return
+
+        try {
+            // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
+            val paymentMethodData = JSONObject(paymentInformation).getJSONObject("paymentMethodData")
+            val billingName = paymentMethodData.getJSONObject("info")
+                .getJSONObject("billingAddress").getString("name")
+            Log.d("BillingName", billingName)
+
+//            Toast.makeText(
+//                this,
+//                getString(R.string.payments_show_name, billingName),
+//                Toast.LENGTH_LONG
+//            ).show()
+
+            // Logging token string.
+            Log.d(
+                "GooglePaymentToken", paymentMethodData
+                    .getJSONObject("tokenizationData")
+                    .getString("token")
+            )
+
+        } catch (e: JSONException) {
+            Log.e("handlePaymentSuccess", "Error: " + e.toString())
+        }
+
+    }
+
+    /**
+     * At this stage, the user has already seen a popup informing them an error occurred. Normally,
+     * only logging is required.
+     *
+     * @param statusCode will hold the value of any constant from CommonStatusCode or one of the
+     * WalletConstants.ERROR_CODE_* constants.
+     * @see [
+     * Wallet Constants Library](https://developers.google.com/android/reference/com/google/android/gms/wallet/WalletConstants.constant-summary)
+     */
+    private fun handleError(statusCode: Int) {
+        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode))
+    }
+
+    private fun fetchRandomGarment() : JSONObject {
+        if (!::garmentList.isInitialized) {
+            garmentList = Json.readFromResources(this, R.raw.tshirts)
+        }
+
+        val randomIndex:Int = Math.round(Math.random() * (garmentList.length() - 1)).toInt()
+        return garmentList.getJSONObject(randomIndex)
+    }
+
+//    private fun displayGarment(garment: JSONObject) {
+//        detailTitle.setText(garment.getString("title"))
+//        detailPrice.setText("\$${garment.getString("price")}")
+//
+//        val escapedHtmlText:String = Html.fromHtml(garment.getString("description")).toString()
+//        detailDescription.setText(Html.fromHtml(escapedHtmlText))
+//
+//        val imageUri = "@drawable/${garment.getString("image")}"
+//        val imageResource = resources.getIdentifier(imageUri, null, packageName)
+//        detailImage.setImageResource(imageResource)
+//    }
+}
